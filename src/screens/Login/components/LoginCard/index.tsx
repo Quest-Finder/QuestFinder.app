f37ff67d99541baf6921579dac879a68442c930e
Adriano <adriano_alves_2008@hotmail.com>
2023-10-19 10:50:43 -0300

    Style: add NativeWind styles

--- src/screens/Login/components/LoginCard/index.tsx
+++ src/screens/Login/components/LoginCard/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 interface LoginCardProps {
@@ -7,25 +7,12 @@ interface LoginCardProps {
 
 const LoginCard = ({ name }: LoginCardProps) => {
   return (
-    <TouchableOpacity style={styles.button}>
-      <Text style={styles.buttonText}>Entrar com {name}</Text>
+    <TouchableOpacity className="bg-gray-400 w-full items-center py-4 rounded-md mb-4">
+      <Text className="text-white font-semibold text-lg">
+        Entrar com {name}
+      </Text>
     </TouchableOpacity>
   )
 }
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: '#989898',
-    width: '100%',
-    alignItems: 'center',
-    paddingVertical: 16,
-    borderRadius: 4
-  },
-  buttonText: {
-    color: '#fff',
-    fontWeight: '600',
-    fontSize: 16
-  }
-})
 
 export default LoginCard
