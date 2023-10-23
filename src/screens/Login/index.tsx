d34f4a0f0330d323ca1ced8f3e38bf1069bbc82d
Iann Rodrigues <iannrst11@gmail.com>
2023-10-22 23:39:32 -0300

    feat: add authentication strategy

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,8 +1,9 @@
-import { Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
-import LoginCard from './components/LoginCard'
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
 
-import Logo from '../../assets/img/logo.svg'
+import Logo from '../../assets/img/logo.svg';
+
+import LoginCard from './components/LoginCard';
 
 const Login = () => {
   return (
@@ -10,15 +11,15 @@ const Login = () => {
       <Logo width={156} height={70} />
       <Text className="font-bold text-lg mt-4 mb-8">Entrar</Text>
       <View className="w-[90%] items-center mb-4">
-        <LoginCard name="Google" />
-        <LoginCard name="Discord" />
-        <LoginCard name="Facebook" />
-        <LoginCard name="Twich" />
+        <LoginCard name="Google" oauth_strategy="oauth_google" />
+        <LoginCard name="Discord" oauth_strategy="oauth_discord" />
+        <LoginCard name="Facebook" oauth_strategy="oauth_facebook" />
+        <LoginCard name="Twitch" oauth_strategy="oauth_twitch" />
       </View>
       <View className="h-px w-60 bg-gray-600 mb-4"></View>
       <Text className="text-gray-800 font-bold text-sm">Precisa de Ajuda?</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
