29896ee3d9efd50f75f96a0fdc06d141145b940f
Iann Rodrigues <iannrst11@gmail.com>
2024-01-01 19:42:10 -0300

    feat: exemplos dias disponíveis na agenda

---
+++ src/components/CalendarSection/example-days.ts
@@ -0,0 +1,78 @@
+import { MarkedDateProps } from './Calendar';
+
+const markedDays: MarkedDateProps = {
+  '2024-01-05': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+  '2024-01-06': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+  '2024-01-12': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+  '2024-01-13': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+  '2024-01-19': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+  '2024-01-20': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+  '2024-01-26': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+  '2024-01-27': {
+    selected: true,
+    selectedColor: '#eefffd',
+    selectedTextColor: '#525252',
+    customTextStyle: {
+      fontWeight: '500',
+      fontSize: 18,
+    },
+  },
+};
+
+export { markedDays };
